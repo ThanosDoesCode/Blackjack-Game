d4e2c9690cb0036e48adbab5320fcb9756bb7ca3
Thanos <98991153+ThanosDoesCode@users.noreply.github.com>
2025-02-17 13:37:18 +0200

    index.js

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ let player = {
     chips: 200
 }
 
-let cards = []
-let sum = 0
+let playerCards = []
+let playerSum = 0
+let dealerCards = []
+let dealerSum = 0
 let hasBlackJack = false
 let isAlive = false 
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let playerSumEl = document.getElementById("player-sum-el")
+let playerCardsEl = document.getElementById("player-cards-el")
 let playerEl = document.getElementById("player-el")
+let dealerCardsEl = document.getElementById("dealer-cards-el")
+let dealerSumEl = document.getElementById("dealer-sum-el")
 
 playerEl.textContent = player.name + ": $" + player.chips
 
@@ -28,23 +32,39 @@ function getRandomCard() {
 
 function startGame() {
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    hasBlackJack = false
+    
+    let playerFirstCard = getRandomCard()
+    let playerSecondCard = getRandomCard()
+    playerCards = [playerFirstCard, playerSecondCard]
+    playerSum = playerFirstCard + playerSecondCard
+    
+    let dealerFirstCard = getRandomCard()
+    let dealerSecondCard = getRandomCard()
+    dealerCards = [dealerFirstCard, dealerSecondCard]
+    dealerSum = dealerFirstCard + dealerSecondCard
+    
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    playerCardsEl.textContent = " Player Cards: "
+    for (let i = 0; i < playerCards.length; i++) {
+        playerCardsEl.textContent += playerCards[i] + " "
     }
     
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
+    if (isAlive === true){
+        dealerCardsEl.textContent = "Dealer Cards: " + dealerCards[0] + " ?"
+        dealerSumEl.textContent = "Dealer Sum: ?"
+    } else {
+        dealerCardsEl.textContent = "Dealer Cards: " + dealerCards.join(" ")
+        dealerSumEl.textContent = "Dealer Sum: " + dealerSum
+    }
+
+    playerSumEl.textContent = "Player Sum: " + playerSum
+    if (playerSum <= 20) {
         message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
+    } else if (playerSum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
@@ -54,12 +74,50 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-
 function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
+    if (isAlive && !hasBlackJack) {
         let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        renderGame()        
+        playerCards.push(card)
+        playerSum += card
+        renderGame()
+
+        if (playerSum >= 21) {
+            endGame()
+        }
     }
+}
+
+// Dealer's turn logic (automatically runs after player stands or busts)
+function dealerTurn() {
+    while (dealerSum < 17) {
+        let card = getRandomCard()
+        dealerCards.push(card)
+        dealerSum += card
+    }
+}
+
+function endGame() {
+    if (playerSum <= 21){
+        dealerTurn()
+    } 
+    renderGame()
+    
+    dealerCardsEl.textContent = "Dealer Cards: " + dealerCards.join(" ")
+    dealerSumEl.textContent = "Dealer Sum: " + dealerSum
+    
+    if (playerSum > 21) {
+        message = "You busted! Dealer wins."
+    } else if (dealerSum > 21 && playerSum === 21) {
+        message = "Blackjack!"
+    } else if (dealerSum > 21){
+        message = "Dealer busted. You win!"
+    } else if (playerSum > dealerSum) {
+        message = "You win!"
+    } else if (playerSum < dealerSum) {
+        message = "Dealer wins!"
+    } else {
+        message = "It's a tie!"
+    }
+
+    messageEl.textContent = message
 }
